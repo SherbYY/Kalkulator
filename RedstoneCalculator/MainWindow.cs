649c7c6df1a3a0dae93f62b34888eb2282fc7052
programmerpeter <programmerpeter.pl@gmail.com>
2016-03-13 16:16:57 +0100

    Much better code Fixed FormatException

--- RedstoneCalculator/MainWindow.cs
+++ RedstoneCalculator/MainWindow.cs
@@ -10,7 +10,7 @@ namespace RedstoneCalculator
             InitializeComponent();
         }
 
-        private void clearAmounts()
+        private void clear(TextBox textBox)
         {
             repeaterAmount.Text = "0";
             comparatorAmount.Text = "0";
@@ -21,209 +21,160 @@ namespace RedstoneCalculator
             pistonAmount.Text = "0";
             stickyPistonAmount.Text = "0";
             noteblockAmount.Text = "0";
+
+            repeaterAmount.Enabled = false;
+            comparatorAmount.Enabled = false;
+            dispenserAmount.Enabled = false;
+            dropperAmount.Enabled = false;
+            redstoneLampAmount.Enabled = false;
+            daylightSensorAmount.Enabled = false;
+            pistonAmount.Enabled = false;
+            stickyPistonAmount.Enabled = false;
+            noteblockAmount.Enabled = false;
+
+            if (textBox != null)
+                textBox.Enabled = true;
+        }
+
+        private void setText(Label label, int number)
+        {
+            label.Text = number.ToString();
+        }
+
+        private int getAmount(TextBox textBox)
+        {
+            try { return int.Parse(textBox.Text); }
+            catch (FormatException) { return 0; }
         }
 
         private void MainWindow_Load(object sender, EventArgs e)
         {
-            repeaterAmount.Enabled = false;
-            comparatorAmount.Enabled = false;
-            dispenserAmount.Enabled = false;
-            dropperAmount.Enabled = false;
-            redstoneLampAmount.Enabled = false;
-            daylightSensorAmount.Enabled = false;
-            pistonAmount.Enabled = false;
-            stickyPistonAmount.Enabled = false;
-            noteblockAmount.Enabled = false;
-            clearAmounts();
+            clear(null);
         }
+
+        #region CheckedChanged
 
         private void repeaterButton_CheckedChanged(object sender, EventArgs e)
         {
-            repeaterAmount.Enabled = true;
-            comparatorAmount.Enabled = false;
-            dispenserAmount.Enabled = false;
-            dropperAmount.Enabled = false;
-            redstoneLampAmount.Enabled = false;
-            daylightSensorAmount.Enabled = false;
-            pistonAmount.Enabled = false;
-            stickyPistonAmount.Enabled = false;
-            noteblockAmount.Enabled = false;
-            clearAmounts();
+            clear(repeaterAmount);
         }
 
         private void comparatorButton_CheckedChanged(object sender, EventArgs e)
         {
-            repeaterAmount.Enabled = false;
-            comparatorAmount.Enabled = true;
-            dispenserAmount.Enabled = false;
-            dropperAmount.Enabled = false;
-            redstoneLampAmount.Enabled = false;
-            daylightSensorAmount.Enabled = false;
-            pistonAmount.Enabled = false;
-            stickyPistonAmount.Enabled = false;
-            noteblockAmount.Enabled = false;
-            clearAmounts();
+            clear(comparatorAmount);
         }
 
         private void dispenserButton_CheckedChanged(object sender, EventArgs e)
         {
-            repeaterAmount.Enabled = false;
-            comparatorAmount.Enabled = false;
-            dispenserAmount.Enabled = true;
-            dropperAmount.Enabled = false;
-            redstoneLampAmount.Enabled = false;
-            daylightSensorAmount.Enabled = false;
-            pistonAmount.Enabled = false;
-            stickyPistonAmount.Enabled = false;
-            noteblockAmount.Enabled = false;
-            clearAmounts();
+            clear(dispenserAmount);
         }
 
         private void dropperButton_CheckedChanged(object sender, EventArgs e)
         {
-            repeaterAmount.Enabled = false;
-            comparatorAmount.Enabled = false;
-            dispenserAmount.Enabled = false;
-            dropperAmount.Enabled = true;
-            redstoneLampAmount.Enabled = false;
-            daylightSensorAmount.Enabled = false;
-            pistonAmount.Enabled = false;
-            stickyPistonAmount.Enabled = false;
-            noteblockAmount.Enabled = false;
-            clearAmounts();
+            clear(dropperAmount);
         }
 
         private void redstoneLampButton_CheckedChanged(object sender, EventArgs e)
         {
-            repeaterAmount.Enabled = false;
-            comparatorAmount.Enabled = false;
-            dispenserAmount.Enabled = false;
-            dropperAmount.Enabled = false;
-            redstoneLampAmount.Enabled = true;
-            daylightSensorAmount.Enabled = false;
-            pistonAmount.Enabled = false;
-            stickyPistonAmount.Enabled = false;
-            noteblockAmount.Enabled = false;
-            clearAmounts();
+            clear(redstoneLampAmount);
         }
 
         private void daylightSensorButton_CheckedChanged(object sender, EventArgs e)
         {
-            repeaterAmount.Enabled = false;
-            comparatorAmount.Enabled = false;
-            dispenserAmount.Enabled = false;
-            dropperAmount.Enabled = false;
-            redstoneLampAmount.Enabled = false;
-            daylightSensorAmount.Enabled = true;
-            pistonAmount.Enabled = false;
-            stickyPistonAmount.Enabled = false;
-            noteblockAmount.Enabled = false;
-            clearAmounts();
+            clear(daylightSensorAmount);
         }
 
         private void pistonButton_CheckedChanged(object sender, EventArgs e)
         {
-            repeaterAmount.Enabled = false;
-            comparatorAmount.Enabled = false;
-            dispenserAmount.Enabled = false;
-            dropperAmount.Enabled = false;
-            redstoneLampAmount.Enabled = false;
-            daylightSensorAmount.Enabled = false;
-            pistonAmount.Enabled = true;
-            stickyPistonAmount.Enabled = false;
-            noteblockAmount.Enabled = false;
-            clearAmounts();
+            clear(pistonAmount);
         }
 
         private void stickyPistonButton_CheckedChanged(object sender, EventArgs e)
         {
-            repeaterAmount.Enabled = false;
-            comparatorAmount.Enabled = false;
-            dispenserAmount.Enabled = false;
-            dropperAmount.Enabled = false;
-            redstoneLampAmount.Enabled = false;
-            daylightSensorAmount.Enabled = false;
-            pistonAmount.Enabled = false;
-            stickyPistonAmount.Enabled = true;
-            noteblockAmount.Enabled = false;
-            clearAmounts();
+            clear(stickyPistonAmount);
         }
 
         private void noteblockButton_CheckedChanged(object sender, EventArgs e)
         {
-            repeaterAmount.Enabled = false;
-            comparatorAmount.Enabled = false;
-            dispenserAmount.Enabled = false;
-            dropperAmount.Enabled = false;
-            redstoneLampAmount.Enabled = false;
-            daylightSensorAmount.Enabled = false;
-            pistonAmount.Enabled = false;
-            stickyPistonAmount.Enabled = false;
-            noteblockAmount.Enabled = true;
-            clearAmounts();
+            clear(noteblockAmount);
         }
+
+        #endregion
+
+        #region TextChanged
 
         private void repeaterAmount_TextChanged(object sender, EventArgs e)
         {
-            redstoneResult.Text = Convert.ToString(int.Parse(repeaterAmount.Text) * 1);
-            redstoneTorchResult.Text = Convert.ToString(int.Parse(repeaterAmount.Text) * 2);
-            stoneResult.Text = Convert.ToString(int.Parse(repeaterAmount.Text) * 3);
+            int amount = getAmount(repeaterAmount);
+            setText(redstoneResult, amount * 1);
+            setText(redstoneTorchResult, amount * 2);
+            setText(stoneResult, amount * 3);
         }
 
         private void comparatorAmount_TextChanged(object sender, EventArgs e)
         {
-            stoneResult.Text = Convert.ToString(int.Parse(comparatorAmount.Text) * 3);
-            redstoneTorchResult.Text = Convert.ToString(int.Parse(comparatorAmount.Text) * 3);
-            quartzResult.Text = Convert.ToString(int.Parse(comparatorAmount.Text) * 1);
+            int amount = getAmount(comparatorAmount);
+            setText(stoneResult, amount * 3);
+            setText(redstoneTorchResult, amount * 3);
+            setText(quartzResult, amount * 1);
         }
 
         private void dispenserAmount_TextChanged(object sender, EventArgs e)
         {
-            cobblestoneResult.Text = Convert.ToString(int.Parse(dispenserAmount.Text) * 7);
-            bowResult.Text = Convert.ToString(int.Parse(dispenserAmount.Text) * 1);
-            redstoneResult.Text = Convert.ToString(int.Parse(dispenserAmount.Text) * 1);
+            int amount = getAmount(dispenserAmount);
+            setText(cobblestoneResult, amount * 7);
+            setText(bowResult, amount * 1);
+            setText(redstoneResult, amount * 1);
         }
 
         private void dropperAmount_TextChanged(object sender, EventArgs e)
         {
-            cobblestoneResult.Text = Convert.ToString(int.Parse(dropperAmount.Text) * 7);
-            redstoneResult.Text = Convert.ToString(int.Parse(dropperAmount.Text) * 1);
+            int amount = getAmount(dropperAmount);
+            setText(cobblestoneResult, amount * 7);
+            setText(redstoneResult, amount * 1);
         }
 
         private void redstoneLampAmount_TextChanged(object sender, EventArgs e)
         {
-            redstoneResult.Text = Convert.ToString(int.Parse(redstoneLampAmount.Text) * 4);
-            glowstoneResult.Text = Convert.ToString(int.Parse(redstoneLampAmount.Text) * 1);
+            int amount = getAmount(redstoneLampAmount);
+            setText(redstoneResult, amount * 4);
+            setText(glowstoneResult, amount * 1);
         }
 
         private void daylightSensorAmount_TextChanged(object sender, EventArgs e)
         {
-            woodenSlabResult.Text = Convert.ToString(int.Parse(daylightSensorAmount.Text) * 3);
-            quartzResult.Text = Convert.ToString(int.Parse(daylightSensorAmount.Text) * 3);
-            glassResult.Text = Convert.ToString(int.Parse(daylightSensorAmount.Text) * 3);
+            int amount = getAmount(daylightSensorAmount);
+            setText(woodenSlabResult, amount * 3);
+            setText(quartzResult, amount * 3);
+            setText(glassResult, amount * 3);
         }
 
         private void pistonAmount_TextChanged(object sender, EventArgs e)
         {
-            cobblestoneResult.Text = Convert.ToString(int.Parse(pistonAmount.Text) * 4);
-            ironIngotResult.Text = Convert.ToString(int.Parse(pistonAmount.Text) * 1);
-            redstoneResult.Text = Convert.ToString(int.Parse(pistonAmount.Text) * 1);
-            woodenPlanksResult.Text = Convert.ToString(int.Parse(pistonAmount.Text) * 3);
+            int amount = getAmount(pistonAmount);
+            setText(cobblestoneResult, amount * 4);
+            setText(ironIngotResult, amount * 1);
+            setText(redstoneResult, amount * 1);
+            setText(woodenPlanksResult, amount * 3);
         }
 
         private void stickyPistonAmount_TextChanged(object sender, EventArgs e)
         {
-            cobblestoneResult.Text = Convert.ToString(int.Parse(stickyPistonAmount.Text) * 4);
-            ironIngotResult.Text = Convert.ToString(int.Parse(stickyPistonAmount.Text) * 1);
-            redstoneResult.Text = Convert.ToString(int.Parse(stickyPistonAmount.Text) * 1);
-            woodenPlanksResult.Text = Convert.ToString(int.Parse(stickyPistonAmount.Text) * 3);
-            slimeResult.Text = Convert.ToString(int.Parse(stickyPistonAmount.Text) * 1);
+            int amount = getAmount(stickyPistonAmount);
+            setText(cobblestoneResult, amount * 4);
+            setText(ironIngotResult, amount * 1);
+            setText(redstoneResult, amount * 1);
+            setText(woodenPlanksResult, amount * 3);
+            setText(slimeResult, amount * 1);
         }
 
         private void noteblockAmount_TextChanged(object sender, EventArgs e)
         {
-            woodenPlanksResult.Text = Convert.ToString(int.Parse(noteblockAmount.Text) * 8);
-            redstoneResult.Text = Convert.ToString(int.Parse(noteblockAmount.Text) * 1);
+            int amount = getAmount(noteblockAmount);
+            setText(woodenPlanksResult, amount * 8);
+            setText(redstoneResult, amount * 1);
         }
+
+        #endregion
     }
 }
